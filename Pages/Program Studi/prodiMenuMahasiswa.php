403650f11351e85515c92198cb15fbfb410453a1
ZeroGyoza <152766914+ZeroGyoza@users.noreply.github.com>
2025-11-24 20:46:01 +0700

    PRODI MENU MAHASISWA (NEW)

--- Pages/Program Studi/prodiMenuMahasiswa.php
+++ Pages/Program Studi/prodiMenuMahasiswa.php
@@ -11,30 +11,43 @@
 <body>
     <?php
     include "../../Assets/Global Components/navbar.php";
+    include "../../SQL/connection.php";
+    require_once "../../config.php";
+    require_once "../../Utils/encryption.php";
+    require_once "../../Utils/gradeUtil.php";
     ?>
     <div id="main">
         <div id="title">
             <h1>Program Studi</h1>
         </div>
+        <?php
+        $getProdiStmt = $con->prepare("SELECT p.kode_prodi, p.nama_prodi, p.fakultas, p.nama_ketua_prodi 
+                                        FROM prodi p INNER JOIN mahasiswa m ON m.kode_prodi = p.kode_prodi
+                                        WHERE m.user_id = ?;");
+        $getProdiStmt->bind_param("s", $_COOKIE["user_id"]);
+        $getProdiStmt->execute();
+        $getProdiResult = $getProdiStmt->get_result();
+        $prodi = $getProdiResult->fetch_assoc();
 
+        ?>
         <div class="container">
             <form method="post">
                 <div id="inputFields">
                     <div id="field">
                         <label>Kode Prodi :</label> 
-                        <input type="text" name="" value="" readonly>
+                        <input type="text" name="kodeProdi" value="<?php echo $prodi['kode_prodi']; ?>" readonly>
                     </div>
                     <div id="field">
                         <label>Nama Prodi :</label>
-                        <input type="text" name="" value="" readonly>
+                        <input type="text" name="namaProdi" value="<?php echo $prodi['nama_prodi']; ?>" readonly>
                     </div>
                     <div id="field">
                         <label>Fakultas Prodi :</label>
-                        <input type="password" name="" value="" readonly>
+                        <input type="text" name="fakultasProdi" value="<?php echo $prodi['fakultas']; ?>" readonly>
                     </div>
                     <div id="field">
                         <label>Ketua Prodi :</label>
-                        <input type = "text" name = "" value="" readonly>
+                        <input type = "text" name = "ketuaProdi" value="<?php echo $prodi['nama_ketua_prodi']; ?>" readonly>
                     </div>
                 </div>
             </form>
